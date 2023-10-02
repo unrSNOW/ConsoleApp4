bd045d011e4de547a24b7a46b351c1534d454882
Asus <Asus@LAPTOP-4QTPOURE>
2023-10-02 19:45:14 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,10 @@
+﻿//3.29.Дано натуральное число n (n > 9). Найти:
+//а)  число единиц в  нем;
+//б)  число десятков в  нем
+
+Console.WriteLine("введите натуральное n:");
+int n = int.Parse(Console.ReadLine());
+int e = n % 10;
+int d = (n / 10) % 10;
+Console.WriteLine($"число едениц: {e}");
+Console.WriteLine($"число десятков: {d}");
